64e0e80afb3e722f0dc4a5d4da273c3e4c6c51ea
MAKINO Gene Takashi <makino@atware.co.jp>
2026-05-18 08:14:41 +0900

    Initial Commit

---
+++ MousePointer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MousePointer
+//
+//  Created by MAKINO Takashi on 2026/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MousePointer/MousePointerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MousePointerApp.swift
+//  MousePointer
+//
+//  Created by MAKINO Takashi on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct MousePointerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
