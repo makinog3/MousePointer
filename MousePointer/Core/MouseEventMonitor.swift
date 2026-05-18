c6bda0e63c77bc9af3e43cb0818f14239cb9b7d4
MAKINO Gene Takashi <makino@atware.co.jp>
2026-05-18 09:03:08 +0900

    feat: MouseEventMonitor CGEventTap wrapper

--- MousePointer/Core/MouseEventMonitor.swift
+++ MousePointer/Core/MouseEventMonitor.swift
@@ -1,4 +1,58 @@
 import Foundation
 import CoreGraphics
 
-// Implemented in Task 3
+final class MouseEventMonitor {
+    var onMouseMoved: ((CGPoint) -> Void)?
+    var onMouseDown:  ((CGPoint) -> Void)?
+
+    private var eventTap: CFMachPort?
+    private var runLoopSource: CFRunLoopSource?
+
+    func start() {
+        let mask: CGEventMask =
+            (1 << CGEventType.mouseMoved.rawValue)       |
+            (1 << CGEventType.leftMouseDragged.rawValue) |
+            (1 << CGEventType.leftMouseDown.rawValue)
+
+        eventTap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .listenOnly,
+            eventsOfInterest: mask,
+            callback: { _, type, event, refcon -> Unmanaged<CGEvent>? in
+                let m = Unmanaged<MouseEventMonitor>
+                    .fromOpaque(refcon!).takeUnretainedValue()
+                m.handle(type: type, event: event)
+                return Unmanaged.passRetained(event)
+            },
+            userInfo: Unmanaged.passUnretained(self).toOpaque()
+        )
+
+        guard let tap = eventTap else { return }
+        runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
+        CFRunLoopAddSource(CFRunLoopGetMain(), runLoopSource, .commonModes)
+        CGEvent.tapEnable(tap: tap, enable: true)
+    }
+
+    func stop() {
+        if let tap = eventTap { CGEvent.tapEnable(tap: tap, enable: false) }
+        if let src = runLoopSource {
+            CFRunLoopRemoveSource(CFRunLoopGetMain(), src, .commonModes)
+        }
+        eventTap = nil
+        runLoopSource = nil
+    }
+
+    private func handle(type: CGEventType, event: CGEvent) {
+        switch type {
+        case .mouseMoved, .leftMouseDragged:
+            onMouseMoved?(event.location)
+        case .leftMouseDown:
+            onMouseDown?(event.location)
+        case .tapDisabledByTimeout, .tapDisabledByUserInput:
+            if let tap = eventTap { CGEvent.tapEnable(tap: tap, enable: true) }
+        default:
+            break
+        }
+    }
+}
